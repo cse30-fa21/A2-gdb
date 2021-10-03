703870b317d6ae8cfcbb256364b247378e5c9121
Erika Auyeung <eauyeung@ucsd.edu>
2021-10-03 14:35:16 -0700

    Remove for loop initial declaration

--- loop.c
+++ loop.c
@@ -5,7 +5,8 @@ int main(int argc, char **argv){
   int sumIt = 0;
   char someStr[] = "Redstone is no substitute for good old C!\n";
   int len=strlen(someStr);
-  for (int i=0; i< 1000; i++){
+  int i;
+  for (i = 0; i< 1000; i++){
     sumIt += someStr[i % len];
   }
   printf("0x%x\n", sumIt);
